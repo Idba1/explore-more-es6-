038488ee80e8c424a3b9508c6fc6fab5e3840ada
Idba1 <moniraislamself@gmail.com>
2024-02-22 12:39:56 +0600

    for-each.js

---
+++ for-each.js
@@ -0,0 +1,11 @@
+const subjects = ['bangla', 'english', 'math']
+const firstLetter = subjects.forEach(sub=>console.log(sub[0]));
+console.log(firstLetter);
+
+
+const numbers = [1, 5, 6, 4, 15];
+// const result = numbers.forEach(n => console.log(n))
+const result = numbers.forEach(n => {
+    
+})
+console.log(result)

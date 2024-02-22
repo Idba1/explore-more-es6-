9339dc9b3696d18debd48127c408984e5ba6e0b9
Idba1 <moniraislamself@gmail.com>
2024-02-22 12:57:32 +0600

    find.js

---
+++ find.js
@@ -0,0 +1,16 @@
+const numbers1 = [1, 5, 6, 4, 15];
+const num = numbers1.find(num => num%2 === 0);
+console.log(num)
+
+const players = [75, 65, 67, 72, 55, 59];
+
+// const selected = players.find(player => player > 70)
+const selected = players.find(player => player > 50)
+console.log(selected)
+
+/**
+ * 1. map 
+ * 2. forEach
+ * 3. filter 
+ * 4. find
+*/

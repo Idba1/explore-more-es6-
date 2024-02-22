12944e2391c5e0846567115194c46bf4acebec69
Idba1 <moniraislamself@gmail.com>
2024-02-22 12:27:27 +0600

    more-map..js

--- more-map..js
+++ more-map..js
@@ -1,3 +1,14 @@
+const subjects = ['bangla', 'english', 'math']
+const firstChar = subjects.map(subject => subject[0])
+console.log(firstChar);
+
+
+/* const marks = [43,53,35,67,76,87,22]
+const avarage = marks.map(mark => (mark+mark)/marks.length);
+console.log(avarage); */
+
+
+
 const numbers = [12, 10, 8, 15, 7];
 
 const doubled = numbers.map(num => num * 2)
